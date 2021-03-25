fa98dcfc5e5c800bd036b58b5a332fa21d2b9a8c
PingChiaHuang <b06902024@ntu.edu.tw>
2021-03-25 21:35:23 +0800

    add other layer

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 import torch
-from torch.nn import Embedding, GRU, Linear, Sequential, Sigmoid
+from torch.nn import Embedding, GRU, Linear, Dropout, LeakyReLU, Tanh, LayerNorm, BatchNorm1d
 
 
 class SeqClassifier(torch.nn.Module):
@@ -18,14 +18,20 @@ class SeqClassifier(torch.nn.Module):
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
         # TODO: model architecture
         embedding_size = embeddings.size(1)
+        self.layernorm = LayerNorm(embedding_size)
         self.gru = GRU(
             embedding_size,
             hidden_size,
             num_layers=num_layers,
-            dropout=dropout,
             bidirectional=bidirectional,
+            batch_first=True,
         )
-        self.classifier = Sequential(Linear(self.encoder_output_size, num_class), Sigmoid())
+        self.leakyrelu = LeakyReLU()
+        self.tanh = Tanh()
+        self.dropout = Dropout(dropout)
+        output_size = hidden_size * (1 + bidirectional)
+        self.batchnorm = BatchNorm1d(output_size)
+        self.fc = Linear(output_size, num_class)
 
         self.hidden_size = hidden_size
         self.bidirectional = bidirectional
@@ -40,8 +46,10 @@ class SeqClassifier(torch.nn.Module):
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
         embedded = self.embed(batch)
-        encoded = self.gru(embedded)
-        last_encoded = encoded[:, -1, :]
-        outputs = self.classifier(last_encoded)
-        return outputs
+        encoded, _ = self.gru(self.layernorm(embedded))
+        encoded = self.dropout(encoded)
+        # last_encoded = torch.cat([encoded[:, -1, :self.hidden_size], encoded[:, 0, self.hidden_size:]], dim=-1)
+        last_encoded = encoded[:, -1]
+        logits = self.fc(last_encoded)
+        return logits
         raise NotImplementedError
