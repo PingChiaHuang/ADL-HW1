8c14d7aa3361a9d7441485e8e1306cb9795c268c
PingChiaHuang <b06902024@ntu.edu.tw>
2021-04-13 19:33:05 +0800

    report

--- b06902024/download.sh
+++ b06902024/download.sh
@@ -4,4 +4,4 @@ wget https://www.dropbox.com/s/7id11bznvky7j1w/046-0.2837-0.9273.pt?dl=1 -O best
 
 # download best slot model
 mkdir -p best_model/slot/init_add_labels
-wget https://www.dropbox.com/s/nrwb6hdg76d15u7/025-0.0318-0.7820.pt?dl=1 -O best_model/slot/init_add_labels/025-0.0318-0.7870.pt
+wget https://www.dropbox.com/s/nrwb6hdg76d15u7/025-0.0318-0.7820.pt?dl=1 -O best_model/slot/init_add_labels/025-0.0318-0.7820.pt
